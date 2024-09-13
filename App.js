3b0fda0f02ce85baeb647a10db6ee435962b1233
saraxixi <saraxi.xixi@gmail.com>
2024-09-12 20:48:46 -0700

    Update the styling and layout

--- App.js
+++ App.js
@@ -3,18 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.background}>
+      {/* Header */}
+      <Text style={styles.header}>WELCOME</Text>
+
+      {/* Form Container */}
+      <View style={styles.container}>
+        <Text style={styles.container}></Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8EC9CE',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  container: {
+    width: '80%',
+    borderRadius: 10,
+    backgroundColor: '#AAAAAA',
+    padding: 20,
+    textAlign: 'center',
+    marginTop: 30,
+
+  },
+  
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#4A00E2',
+    textAlign: 'center',
+    marginTop: 80,
   },
 });
